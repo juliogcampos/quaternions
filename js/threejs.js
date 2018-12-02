722e0ba5549c20964f3abac2e4bb3b909ddecee7
Júlio Campos <juliogcampos@gmail.com>
2018-12-02 11:23:37 -0200

    Criação da página three.js e outras alterações

--- js/threejs.js
+++ js/threejs.js
@@ -7,11 +7,12 @@ var quaternion = new THREE.Quaternion();
 
 // definir rotação do quatérnio por um eixo e ângulo
 // o eixo é considerado normalizado, o ângulo é dado em radianos
-quaternion.setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), Math.PI / 2 );
+// o eixo y foi definido para a rotação
+quaternion.setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI / 2);
 
-//
-var vector = new THREE.Vector3( 1, 0, 0 );
-vector.applyQuaternion( quaternion );
+// criar um vetor 3D (x,y,z) e aplicar rotação
+var vector = new THREE.Vector3(1, 0, 0);
+vector.applyQuaternion(quaternion);
 
 document.getElementById("quaternion").innerHTML = JSON.stringify(quaternion, null, "	");
 document.getElementById("vector").innerHTML = JSON.stringify(vector, null, "	");
