c6caaa0971df1b787aaf413861eded9b558bb15b
Júlio Gonçalves Campos <juliogcampos@tecgraf.puc-rio.br>
2018-12-04 10:28:24 -0200

    Alterações na página de luzes

--- js/luzes.js
+++ js/luzes.js
@@ -2,9 +2,11 @@
 if (WEBGL.isWebGLAvailable() === false) {
     document.body.appendChild(WEBGL.getWebGLErrorMessage());
 }
+
 var camera, scene, renderer,
     bulbLight, bulbMat, hemiLight, stats;
 var ballMat, cubeMat, floorMat;
+
 // ref for lumens: http://www.power-sure.com/lumens.htm
 var bulbLuminousPowers = {
     "110000 lm (1000W)": 110000,
@@ -16,6 +18,7 @@ var bulbLuminousPowers = {
     "20 lm (4W)": 20,
     "Off": 0
 };
+
 // ref for solar irradiances: https://en.wikipedia.org/wiki/Lux
 var hemiLuminousIrradiances = {
     "0.0001 lx (Moonless Night)": 0.0001,
@@ -30,12 +33,14 @@ var hemiLuminousIrradiances = {
     "18000 lx (Daylight)": 18000,
     "50000 lx (Direct Sun)": 50000
 };
+
 var params = {
     shadows: true,
     exposure: 0.68,
     bulbPower: Object.keys(bulbLuminousPowers)[4],
     hemiIrradiance: Object.keys(hemiLuminousIrradiances)[0]
 };
+
 init();
 animate();
 
@@ -47,6 +52,7 @@ function init() {
     camera.position.x = -4;
     camera.position.z = 4;
     camera.position.y = 2;
+
     scene = new THREE.Scene();
     var bulbGeometry = new THREE.SphereBufferGeometry(0.02, 16, 8);
     bulbLight = new THREE.PointLight(0xffee88, 1, 100, 2);
@@ -55,20 +61,24 @@ function init() {
         emissiveIntensity: 1,
         color: 0x000000
     });
+
     bulbLight.add(new THREE.Mesh(bulbGeometry, bulbMat));
     bulbLight.position.set(0, 2, 0);
     bulbLight.castShadow = true;
     scene.add(bulbLight);
+
     hemiLight = new THREE.HemisphereLight(0xddeeff, 0x0f0e0d, 0.02);
     scene.add(hemiLight);
+
     floorMat = new THREE.MeshStandardMaterial({
         roughness: 0.8,
         color: 0xffffff,
         metalness: 0.2,
         bumpScale: 0.0005
     });
+
     var textureLoader = new THREE.TextureLoader();
-    textureLoader.load("textures/hardwood2_diffuse.jpg", function(map) {
+    textureLoader.load("img/hardwood2_diffuse.jpg", function (map) {
         map.wrapS = THREE.RepeatWrapping;
         map.wrapT = THREE.RepeatWrapping;
         map.anisotropy = 4;
@@ -76,7 +86,8 @@ function init() {
         floorMat.map = map;
         floorMat.needsUpdate = true;
     });
-    textureLoader.load("textures/hardwood2_bump.jpg", function(map) {
+
+    textureLoader.load("img/hardwood2_bump.jpg", function (map) {
         map.wrapS = THREE.RepeatWrapping;
         map.wrapT = THREE.RepeatWrapping;
         map.anisotropy = 4;
@@ -84,7 +95,8 @@ function init() {
         floorMat.bumpMap = map;
         floorMat.needsUpdate = true;
     });
-    textureLoader.load("textures/hardwood2_roughness.jpg", function(map) {
+
+    textureLoader.load("img/hardwood2_roughness.jpg", function (map) {
         map.wrapS = THREE.RepeatWrapping;
         map.wrapT = THREE.RepeatWrapping;
         map.anisotropy = 4;
@@ -92,13 +104,15 @@ function init() {
         floorMat.roughnessMap = map;
         floorMat.needsUpdate = true;
     });
+
     cubeMat = new THREE.MeshStandardMaterial({
         roughness: 0.7,
         color: 0xffffff,
         bumpScale: 0.002,
         metalness: 0.2
     });
-    textureLoader.load("textures/brick_diffuse.jpg", function(map) {
+
+    textureLoader.load("img/brick_diffuse.jpg", function (map) {
         map.wrapS = THREE.RepeatWrapping;
         map.wrapT = THREE.RepeatWrapping;
         map.anisotropy = 4;
@@ -106,7 +120,8 @@ function init() {
         cubeMat.map = map;
         cubeMat.needsUpdate = true;
     });
-    textureLoader.load("textures/brick_bump.jpg", function(map) {
+
+    textureLoader.load("img/brick_bump.jpg", function (map) {
         map.wrapS = THREE.RepeatWrapping;
         map.wrapT = THREE.RepeatWrapping;
         map.anisotropy = 4;
@@ -114,45 +129,54 @@ function init() {
         cubeMat.bumpMap = map;
         cubeMat.needsUpdate = true;
     });
+
     ballMat = new THREE.MeshStandardMaterial({
         color: 0xffffff,
         roughness: 0.5,
         metalness: 1.0
     });
-    textureLoader.load("textures/planets/earth_atmos_2048.jpg", function(map) {
+
+    textureLoader.load("img/planets/earth_atmos_2048.jpg", function (map) {
         map.anisotropy = 4;
         ballMat.map = map;
         ballMat.needsUpdate = true;
     });
-    textureLoader.load("textures/planets/earth_specular_2048.jpg", function(map) {
+
+    textureLoader.load("img/planets/earth_specular_2048.jpg", function (map) {
         map.anisotropy = 4;
         ballMat.metalnessMap = map;
         ballMat.needsUpdate = true;
     });
+
     var floorGeometry = new THREE.PlaneBufferGeometry(20, 20);
     var floorMesh = new THREE.Mesh(floorGeometry, floorMat);
     floorMesh.receiveShadow = true;
     floorMesh.rotation.x = -Math.PI / 2.0;
     scene.add(floorMesh);
+
     var ballGeometry = new THREE.SphereBufferGeometry(0.25, 32, 32);
     var ballMesh = new THREE.Mesh(ballGeometry, ballMat);
     ballMesh.position.set(1, 0.25, 1);
     ballMesh.rotation.y = Math.PI;
     ballMesh.castShadow = true;
     scene.add(ballMesh);
+
     var boxGeometry = new THREE.BoxBufferGeometry(0.5, 0.5, 0.5);
     var boxMesh = new THREE.Mesh(boxGeometry, cubeMat);
     boxMesh.position.set(-0.5, 0.25, -1);
     boxMesh.castShadow = true;
     scene.add(boxMesh);
+
     var boxMesh2 = new THREE.Mesh(boxGeometry, cubeMat);
     boxMesh2.position.set(0, 0.25, -5);
     boxMesh2.castShadow = true;
     scene.add(boxMesh2);
+
     var boxMesh3 = new THREE.Mesh(boxGeometry, cubeMat);
     boxMesh3.position.set(7, 0.25, 0);
     boxMesh3.castShadow = true;
     scene.add(boxMesh3);
+
     renderer = new THREE.WebGLRenderer();
     renderer.physicallyCorrectLights = true;
     renderer.gammaInput = true;
@@ -162,6 +186,7 @@ function init() {
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     container.appendChild(renderer.domElement);
+
     var controls = new THREE.OrbitControls(camera, renderer.domElement);
     window.addEventListener('resize', onWindowResize, false);
     var gui = new dat.GUI();
@@ -177,11 +202,12 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
-//
+
 function animate() {
     requestAnimationFrame(animate);
     render();
 }
+
 var previousShadowMap = false;
 
 function render() {
@@ -202,4 +228,3 @@ function render() {
     renderer.render(scene, camera);
     stats.update();
 }
-		
